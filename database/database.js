eb99d0615782a94efe2c813e4891fc1383d9d5f1
Saibe <sebastien.cuvellier@hotmail.com>
2020-12-19 16:37:35 +0100

    🚧 Add message event

--- database/database.js
+++ database/database.js
@@ -2,16 +2,41 @@ const sqlite3 = require('sqlite3').verbose();
 
 class Database {
 
-    static databasePath = "database.db";
+    static databasePath = 'database/database.db';
+
+    database;
 
     constructor(){
 
-        let db = new sqlite3.Database(Database.databasePath, (err) => {
+        this.database = new sqlite3.Database(Database.databasePath, (err) => {
             if (err) {
               return console.error(err.message);
             }
-            console.log('Connected to the database.');
           });
+    }
+
+    getAdmins(){
+        return new Promise((resolve, reject) => {
+            let result = []
+            this.database.each(`SELECT * FROM Roles WHERE Admin=1;`, (err, row) => {
+              if(err) { reject(err) }
+              result.push(row.ID)
+            }, () => {
+              resolve(result)
+            });
+        });
+    }
+
+    getChannelsWith(permission){
+        return new Promise((resolve, reject) => {
+            let result = []
+            this.database.each(`SELECT ID_Channel FROM Channel_Permissions where ID_Permission ='${permission}';`, (err, row) => {
+              if(err) { reject(err) }
+              result.push(row.ID_Channel)
+            }, () => {
+              resolve(result)
+            })
+        });
     }
 
 }
---
+++ events/client/message.js
@@ -0,0 +1,100 @@
+const Database = require("../../database/database.js");
+const { PREFIX } = require("../../config.json");
+const { MessageEmbed } = require('discord.js');
+
+
+module.exports = (client, message) => {
+    
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.help.aliases && cmd.help.aliases.includes(commandName));
+    if (!command) return;
+
+    
+    const db = new Database();
+    let isAdmin = false;
+    db.getAdmins().then((ids) => {
+
+        ids.forEach(id => {
+            if (message.member.roles.cache.has(id)) {
+                isAdmin = true;
+            }
+        });
+
+        if(adminCommands(command, message, isAdmin)){
+            return;
+        }
+
+        channelCheck(message, isAdmin, db).then((cmd) => {
+            if(cmd){
+                return;
+            }
+
+            if (command.help.args && !args.length) {
+                errorMessage(command, message);
+                return;
+            }
+        
+            if (command.help.botIdArg && !(args[0] === `<@!${client.user.id}>` || args[0] === `<@${client.user.id}>` || args[0].toLowerCase() === client.user.username.toLowerCase())) {
+                return;
+            }
+        
+            command.run(client, message, args);
+
+        });
+       
+        
+
+    });
+
+}
+
+const errorMessage = (command, message) => {
+    const messageError = new MessageEmbed()
+        .setColor('RED')
+        .setTitle(":x: Commande")
+        .setDescription(`Merci d'utiliser la commande correctement:\n${PREFIX} ${command.help.name} {${command.help.usage}}`);
+    message.channel.send(messageError).then( msg =>{
+        msg.delete({ timeout: 30000 }); 
+    });
+}
+
+
+const adminCommands = (command, message, isAdmin) => {
+    if (!(message.member.hasPermission('ADMINISTRATOR') || isAdmin) && command.help.admin) {
+        const messagePing = new MessageEmbed()
+            .setColor('RED')
+            .setTitle(":x: Commande")
+            .setDescription(`Vous n'avez pas accès à cette commande.`);
+        message.channel.send(messagePing).then( msg => {
+            msg.delete({ timeout: 30000 }); 
+        });
+        return true;
+    }
+    return false;
+}
+
+const channelCheck = (message, isAdmin, db) => {
+    return new Promise((resolve, reject) => {
+        let cmd = false;
+        if (!(message.member.hasPermission('ADMINISTRATOR') || isAdmin)) {
+            db.getChannelsWith('commands').then((ids) => {
+                ids.forEach(id => {
+                    if (message.channel.id === id) {
+                        cmd = true;
+                    }
+                });
+
+                if(!cmd){
+                    message.delete();
+                }
+                resolve(!cmd);
+            });
+        }else{
+            resolve(cmd);
+        }
+    });
+}
